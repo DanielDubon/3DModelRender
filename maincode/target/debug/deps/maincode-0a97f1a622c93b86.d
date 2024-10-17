a44544ddc4a478e42da4c372384829a8aa771a25
DanielDubon <dub22233@uvg.edu.gt>
2024-10-17 16:33:23 -0600

    feat: init cargo

---
+++ maincode/target/debug/deps/maincode-0a97f1a622c93b86.d
@@ -0,0 +1,5 @@
+/home/danidubon12/3DModelRender/maincode/target/debug/deps/maincode-0a97f1a622c93b86: src/main.rs
+
+/home/danidubon12/3DModelRender/maincode/target/debug/deps/maincode-0a97f1a622c93b86.d: src/main.rs
+
+src/main.rs:
---
+++ maincode/target/debug/maincode.d
@@ -0,0 +1,1 @@
+/home/danidubon12/3DModelRender/maincode/target/debug/maincode: /home/danidubon12/3DModelRender/maincode/src/main.rs
